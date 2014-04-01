bb0d8699ebb76a0f81e4c8d38b9666f29d77735c
pilgrim <iapilgrim@gmail.com>
2014-04-01 15:56:02 +0700

    correct hello world cookbook

--- cookbooks/helloworld/attributes/me.rb
+++ cookbooks/helloworld/attributes/me.rb
@@ -1,4 +1,4 @@
-set[:about_me][:first_name] = "<INSERT_YOUR_NAME_HERE>"
-default[:about_me][:favorite][:food] = "pizza"
-default[:about_me][:favorite][:drink] = "water"
-default[:about_me][:favorite][:hobby] = "running"
+set[:helloworld][:first_name] = "<INSERT_YOUR_NAME_HERE>"
+default[:helloworld][:favorite][:food] = "pizza"
+default[:helloworld][:favorite][:drink] = "water"
+default[:helloworld][:favorite][:hobby] = "running"
--- cookbooks/helloworld/recipes/default.rb
+++ cookbooks/helloworld/recipes/default.rb
@@ -8,9 +8,9 @@
 #
 rightscale_marker
 
-  first_name = node[:about_me][:first_name]
-  food = node[:about_me][:favorite][:food]
-  drink = node[:about_me][:favorite][:drink]
-  hobby = node[:about_me][:favorite][:hobby]
+  first_name = node[:helloworld][:first_name]
+  food = node[:helloworld][:favorite][:food]
+  drink = node[:helloworld][:favorite][:drink]
+  hobby = node[:helloworld][:favorite][:hobby]
   
   log "Hi, my name is #{first_name} and I like #{food}, #{drink} and #{hobby}."
